22fb93fcbf3de2881038c0be20a6cfdf95cdfe79
SachinSaiTej <saitejakm2001@gmail.com>
2024-02-25 00:09:20 +0530

    Initial Commit.

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+
+function NavBar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
+    return (
+        <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+            <Menu setActive={setActive}>
+                <Link href={"/"}>
+                    <MenuItem setActive={setActive} active={active} item="Home">
+                    </MenuItem>
+                </Link>
+                <MenuItem setActive={setActive} active={active} item="Our Courses">
+                    <div className="flex space-y-4 flex-col text-sm">
+                        <HoveredLink href="/courses"> All Courses </HoveredLink>
+                        <HoveredLink href="/courses"> Basic Music Theory </HoveredLink>
+                        <HoveredLink href="/courses"> Advanced Composition </HoveredLink>
+                        <HoveredLink href="/courses"> Song-writing </HoveredLink>
+                        <HoveredLink href="/courses"> Music Production </HoveredLink>
+                    </div>
+                </MenuItem>
+                <Link href={"/contact"}>
+                    <MenuItem setActive={setActive} active={active} item="Contact Us">
+                        test
+                    </MenuItem>
+                </Link>
+            </Menu>
+        </div>
+    );
+}
+
+export default NavBar;
